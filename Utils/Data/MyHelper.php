bd0410b9287ac9d74ddfeb994379dbca86232665
xiedali <notinmood@gmail.com>
2021-07-12 16:45:20 +0800

    4.0.2

--- Utils/Data/MyHelper.php
+++ Utils/Data/MyHelper.php
@@ -18,8 +18,8 @@ class MyHelper
     //第1.2.2版本
     //第2.0.1版本
     //第3.0.0版本
-    //第3.2.7-RC版本
+    //第4.0.2版本
     public static function getVersion(){
-        return "3.2.7-RC";
+        return "4.0.2";
     }
 }
